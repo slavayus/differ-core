f30dcb023fa07ed4cb274e0d66ddcb25a999245c
v.iusiumbeli <slavayus@gmail.com>
2019-09-23 23:11:11 +0300

    initial commit

---
+++ src/main/kotlin/com/differ/differcore/Controller.kt
@@ -0,0 +1,15 @@
+package com.differ.differcore
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.text.SimpleDateFormat
+import java.util.*
+
+@RestController
+class Controller {
+    @GetMapping(value = ["/"])
+    fun home(): String {
+        val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
+        return simpleDateFormat.format(Date())
+    }
+}
