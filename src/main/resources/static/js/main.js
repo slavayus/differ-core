7514636d2fa9272f5df4b8840038ea7cfa26e465
v.iusiumbeli <slavayus@gmail.com>
2019-11-17 19:34:20 +0300

    add expand button icons

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
     $('.block-tag').click(function () {
         $(this).parents(".block-tag-section").find(".block-content").toggle();
+        setupExpandOperationSvgIcon(this);
     });
 });
+
+function setupExpandOperationSvgIcon(element) {
+    let useTag = $(element).parents(".block-tag-section").find("use");
+    let currentSvg = useTag.attr('href');
+    if (currentSvg === "#large-arrow-down") {
+        useTag.attr('href', '#large-arrow');
+    } else {
+        useTag.attr('href', '#large-arrow-down');
+    }
+}
