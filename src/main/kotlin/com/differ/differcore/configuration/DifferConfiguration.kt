20bb16cb6496d35068b3f2b7eda92a00660c26d0
v.iusiumbeli <slavayus@gmail.com>
2019-12-30 12:50:04 +0300

    removed unnecessary differ configuration

--- src/main/kotlin/com/differ/differcore/configuration/DifferConfiguration.kt
+++ src/main/kotlin/com/differ/differcore/configuration/DifferConfiguration.kt
@@ -1,9 +1,6 @@
 package com.differ.differcore.configuration
 
-import freemarker.template.DefaultObjectWrapperBuilder
-import freemarker.template.TemplateHashModel
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
@@ -12,8 +9,4 @@ import org.springframework.context.annotation.PropertySource
 @Configuration
 @ComponentScan(basePackages = ["com.differ.differcore"])
 @PropertySource(value = ["classpath:application.properties"])
-open class DifferConfiguration {
-    @Bean
-    open fun freemarkerStatics(): TemplateHashModel =
-        DefaultObjectWrapperBuilder(freemarker.template.Configuration.VERSION_2_3_29).build().staticModels
-}
+open class DifferConfiguration
